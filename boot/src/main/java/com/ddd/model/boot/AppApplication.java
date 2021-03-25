a8d94c0d84178432a93a998399b9d31ecbff4dd3
阳君 <937447974@qq.com>
2021-03-25 11:14:19 +0800

    feat: 添加架构图

---
+++ boot/src/main/java/com/ddd/model/boot/AppApplication.java
@@ -0,0 +1,11 @@
+package com.ddd.model.boot;
+
+public class AppApplication {
+
+    public static void main(String[] args) {
+        try {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
